c6c96e20485584d8cf5e6cbfe7ff51af4c2b80be
aritdas274 <aritdas274@gmail.com>
2025-05-17 03:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function animateBox() {
+      document.getElementById('button').classList.toggle('animate1');
+      document.getElementById('image').classList.toggle('animate3');
+      document.getElementById('deb').classList.toggle('animate2');
+      document.getElementById('question').classList.toggle('animate4');
+      document.getElementById('box1').classList.toggle('animate5');
+    }
+
+function next1() {
+      document.getElementById("box1").style.zIndex = "1";
+      document.getElementById('box2').classList.toggle('animate3');
+}
+
+function next2() {
+      document.getElementById("box2").style.zIndex = "2";
+      document.getElementById('box3').classList.toggle('animate3');
+}
+
+function next3() {
+      document.getElementById("box3").style.zIndex = "3";
+      document.getElementById('box4').classList.toggle('animate3');
+}
+
+function next4() {
+      document.getElementById("box4").style.zIndex = "4";
+      document.getElementById('box5').classList.toggle('animate3');
+}
+
+function next5() {
+      document.getElementById("box5").style.zIndex = "5";
+      document.getElementById('box6').classList.toggle('animate3');
+}
